c576f69d70930636dcf5b4bb21c80918a9a732fb
rv-13 <r131194@gmail.com>
2022-11-18 01:08:04 +0530

    Automation Tests developed_updated

---
+++ src/test/java/Tests_PATCH_Method.java
@@ -0,0 +1,32 @@
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
+public class Tests_PATCH_Method {
+    private static String patchUrl = "https://reqres.in/api/users/2";
+
+    @Test
+    public void test_01() {
+
+        HashMap<String, Object> map = new HashMap<>();
+
+        JSONObject requestbody = new JSONObject();
+        requestbody.put("name", "morpheusUpdated");
+        requestbody.put("job", "leader");
+        System.out.println(requestbody);
+
+        given()
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(requestbody.toJSONString())
+                .when()
+                .patch(patchUrl)
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+}
---
+++ src/test/java/Tests_PUT_Method.java
@@ -0,0 +1,55 @@
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
+public class Tests_PUT_Method {
+    private static String putUrl = "https://reqres.in/api/users/2";
+
+
+    @Test
+    public void test_01() {
+
+        HashMap<String, Object> map = new HashMap<>();
+
+        JSONObject requestbody = new JSONObject();
+        requestbody.put("name", "morpheus");
+        requestbody.put("job", "leader");
+        System.out.println("Payload created :-" + requestbody);
+
+        given()
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(requestbody.toJSONString())
+                .when()
+                .put(putUrl)
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    public void test_02() {
+
+        HashMap<String, Object> map = new HashMap<>();
+
+        JSONObject requestbody = new JSONObject();
+        requestbody.put("name", "morpheus1");
+        requestbody.put("job", "leader");
+        System.out.println("Payload created :-" + requestbody);
+
+        given()
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(requestbody.toJSONString())
+                .when()
+                .put(putUrl)
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+
+}

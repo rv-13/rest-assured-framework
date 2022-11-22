26f50e25d740174d638d32bb64d91d5fd914ff61
rv-13 <r131194@gmail.com>
2022-11-23 01:35:51 +0530

    Automation Tests developed with data driven flows

--- src/test/java/DataDrivenFlow.java
+++ src/test/java/DataDrivenFlow.java
@@ -8,25 +8,11 @@ import static io.restassured.RestAssured.given;
 public class DataDrivenFlow {
 
     private static String postUrl = "https://reqres.in/api/users";
+    private static String deleteUrl = "https://reqres.in/api/users/";
 
-    @DataProvider(name = "dataProviderForPostcalls")
-    public Object[][] dataProvider() {
-
-        return new Object[][]{
-                {"Ram1", "General Manager"},
-                {"Shyam1", "CEO"},
-                {"Lakshman1", "Leader"},
-                {"Ram2", "General Manager"},
-                {"Shyam2", "CEO"},
-                {"Lakshman2", "Leader"},
-                {"Ram3", "General Manager"},
-                {"Shyam3", "CEO"},
-                {"Lakshman3", "Leader"}
-        };
-    }
 
     @Test(dataProvider = "dataProviderForPostcalls")
-    public void test_01(String name, String job) {
+    public void test_01_post(String name, String job) {
         JSONObject requestbody = new JSONObject();
         requestbody.put("name", name);
         requestbody.put("job", job);
@@ -42,5 +28,41 @@ public class DataDrivenFlow {
                 .statusCode(201)
                 .log().all();
 
+    }
+
+
+    @Test(dataProvider = "dataProviderForDeleteCalls")
+    public void test_01_delete(int id) {
+
+        given()
+                .delete(deleteUrl + id)
+                .then()
+                .statusCode(204)
+                .log().all();
+    }
+
+    @DataProvider(name = "dataProviderForDeleteCalls")
+    public Object[] dataProviderForDelete() {
+
+        return new Object[]{
+                1, 2, 3, 4, 5
+
+        };
+    }
+
+    @DataProvider(name = "dataProviderForPostcalls")
+    public Object[][] dataProviderForPost() {
+
+        return new Object[][]{
+                {"Ram1", "General Manager"},
+                {"Shyam1", "CEO"},
+                {"Lakshman1", "Leader"},
+                {"Ram2", "General Manager"},
+                {"Shyam2", "CEO"},
+                {"Lakshman2", "Leader"},
+                {"Ram3", "General Manager"},
+                {"Shyam3", "CEO"},
+                {"Lakshman3", "Leader"}
+        };
     }
 }
